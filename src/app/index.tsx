e3701abdad0b111504d7e9b5a4b844a2252d9956
Junieslon <juniosantos_@hotmail.com>
2026-02-26 12:30:04 -0400

    [UPDATE]: Remove SEO component from the application and refactor index.tsx to streamline the rendering structure by eliminating unnecessary fragments.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,29 +8,25 @@ import { FAQ } from '../components/FAQ';
 import { AppointmentForm } from '../components/Appointment';
 import { Footer } from '../components/Footer';
 import { Testimonials } from '../components/Testimonials';
-import { SEO } from '../components/SEO';
 import { useInitData } from '../hooks/useInitData';
 
 function App() {
 
   useInitData();
-  
+
   return (
-    <>
-      <SEO />
-      <div className="min-h-screen bg-white">
-        <Header />
-        <Hero />
-        <About />
-        <Services />
-        <Booking/>
-        <Gallery />
-        <FAQ />
-        <Testimonials/>
-        <AppointmentForm />
-        <Footer />
-      </div>
-    </>
+    <div className="min-h-screen bg-white">
+      <Header />
+      <Hero />
+      <About />
+      <Services />
+      <Booking />
+      <Gallery />
+      <FAQ />
+      <Testimonials />
+      <AppointmentForm />
+      <Footer />
+    </div>
   );
 }
 
