222f1aeba26c70b621efc4b69cd7c8bd2726551c
Junieslon <juniosantos_@hotmail.com>
2026-03-21 15:58:03 -0400

    feat: Adapt navigation with react-router and route control.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,31 +1,45 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
+import { Link, useLocation } from 'react-router-dom';
 
 import { NavLinks } from './NavLinks';
 import { MobileMenu } from './MobileMenu';
 
 import Logo from '/logo.svg'
 
+const MotionLink = motion(Link);
+
 export const Header = () => {
+  const location = useLocation();
+
   const [isOpen, setIsOpen] = useState(false);
+
+  const isTeethWhitening =
+    location.pathname !== '/clareamento-dental-caseiro';
 
   return (
     <header className="fixed w-full bg-white/90 backdrop-blur-sm z-50 shadow-sm">
       <section className="container mx-auto px-4 md:px-6 py-4">
         <div className="flex items-center justify-between">
-          <motion.a
-            href="#inicio"
+          <MotionLink
+            to="/"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             className="flex items-center space-x-2"
           >
             <img src={Logo} className="text-purple-600 w-9 h-9" />
             <span className="text-xl font-bold text-gray-900">Dra. Estefany</span>
-          </motion.a>
+          </MotionLink>
 
-          <NavLinks className="hidden md:flex space-x-8" />
+          {isTeethWhitening &&
+            <>
+              <NavLinks className="hidden md:flex space-x-8" />
+              <MobileMenu isOpen={isOpen} setIsOpen={setIsOpen} />
+            </>
+          }
 
-          <MobileMenu isOpen={isOpen} setIsOpen={setIsOpen} />
+
+
         </div>
       </section>
     </header>
