029b97197a32a7ae793dbd84629ba331dc200a48
Junieslon <juniosantos_@hotmail.com>
2026-03-21 16:07:39 -0400

    fix(utils): Adjusts phone mask to format with 9th digit.

--- src/utils/phoneMask.ts
+++ src/utils/phoneMask.ts
@@ -1,8 +1,7 @@
 export function phoneMask(value: string) {
-    return value
-      .replace(/\D/g, "")              // remove tudo que não é número
-      .replace(/^(\d{2})(\d)/, "($1) $2") // (92) 9
-      .replace(/(\d{4})(\d)/, "$1-$2")    // 9999-9999
-      .slice(0, 15);                    // limite máximo
-  }
-  
+  return value
+    .replace(/\D/g, "")
+    .replace(/^(\d{2})(\d)/, "($1) $2")     // (92) 9
+    .replace(/(\d{1})(\d{4})(\d{4})$/, "$1 $2-$3") // 9 9459-3970
+    .slice(0, 16);
+}
